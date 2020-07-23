685351ab556a85163c92f9927525d5333eea032f
javaee <javaee@javaee-VirtualBox>
2020-07-23 14:02:32 +0200

    Funcionalidad crear/modificar FrontOffice incorporando verificacion para asegurar usuario que crea/ modifica. TODO perfume modificado volver a estado de "pendiente".

--- src/main/java/controller/frontoffice/CrearPerfumeFrontOfficeController.java
+++ src/main/java/controller/frontoffice/CrearPerfumeFrontOfficeController.java
@@ -18,6 +18,7 @@ import org.apache.log4j.Logger;
 
 
 import controller.Message;
+import modelo.dao.SeguridadException;
 import modelo.dao.impl.MarcaDAOImpl;
 import modelo.dao.impl.PerfumeDAOImpl;
 import modelo.pojo.Marca;
@@ -40,25 +41,39 @@ public class CrearPerfumeFrontOfficeController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			Perfume perfume = new Perfume();
+		// recuperar usuario de session y setearlo en el producto
+			HttpSession session = request.getSession();
+			Usuario usuario = (Usuario)session.getAttribute("Usuario_login");
+			perfume.setUsuario(usuario);
+			String view = "formulario.jsp";
+		
 		
 		try {
 			String idParameter = request.getParameter("id");
-			Perfume perfume = new Perfume();
 			
 			if (idParameter != null) {
-				int id = Integer.parseInt(idParameter);
-				request.setAttribute("perfume", daoPerfume.getById(id));
+				int idPerfume = Integer.parseInt(idParameter);
+				int idUsuario = usuario.getId();
+				perfume = daoPerfume.getById(idPerfume, idUsuario);
 				request.setAttribute("tittle", "Formulario para Modificar perfume");
+			
 			}else {
 				request.setAttribute("perfume", perfume);	
 				request.setAttribute("tittle", "Formulario Crear perfume");
 			}
 			
+		}catch (SeguridadException e) {
+			view = "/views/frontoffice/inicio";
+			LOG.error("Se estan saltando la seguridad " + usuario);
+			
 		} catch (Exception e) {
 			LOG.error(e);
+			
 		}finally {
+			request.setAttribute("perfume", perfume);	
 			request.setAttribute("marcas", daoMarca.getAll());
-			request.getRequestDispatcher("formulario.jsp").forward(request, response);
+			request.getRequestDispatcher(view).forward(request, response);
 		}
 		
 	}
@@ -67,6 +82,11 @@ public class CrearPerfumeFrontOfficeController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//creo mis nuevos atributos
+		Perfume perfume = new Perfume();
+		Marca marca = new Marca();
+		Message message = new Message();
+		String view = "formulario.jsp";
 		
 		//recupero los parametros
 		String idParameter = request.getParameter("id");
@@ -76,38 +96,42 @@ public class CrearPerfumeFrontOfficeController extends HttpServlet {
 		String imagen = request.getParameter("imagen");
 		String marcaId = request.getParameter("marca_id");
 		
-		
-		int id = Integer.parseInt(idParameter);
-		int idMarca = Integer.parseInt(marcaId);
-		int ml = Integer.parseInt(mlParameter);
-		float precio = Float.parseFloat(precioParameter);
-		
-		//creo mis nuevos atributos
-		Perfume perfume = new Perfume();
-		Marca marca = new Marca();
-		Message message = new Message();
-		
-		perfume.setId(id);
-		perfume.setNombre(nombre);
-		perfume.setMl(ml);
-		perfume.setPrecio(precio);
-		perfume.setImagen(imagen);
-		marca.setId(idMarca);
-		perfume.setMarca(marca);
-		
 		// recuperar usuario de session y setearlo en el producto
 		HttpSession session = request.getSession();
 		Usuario usuario = (Usuario)session.getAttribute("Usuario_login");
 		perfume.setUsuario(usuario);
 		
-		Set<ConstraintViolation<Perfume>> violations = validator.validate(perfume);
-		
 		
 		try {
 			
+			int idPerfume = Integer.parseInt(idParameter);
+			int idUsuario = usuario.getId();
+		
+			if (idPerfume !=0) {
+				perfume = daoPerfume.getById(idPerfume, idUsuario);// verifico nuevamente que elperfume es del usuario registrado, si no, salta la seguridad
+			}
+			
+			
+			int idMarca = Integer.parseInt(marcaId);
+			int ml = Integer.parseInt(mlParameter);
+			float precio = Float.parseFloat(precioParameter);
+			
+			perfume.setId(idPerfume);
+			perfume.setNombre(nombre);
+			perfume.setMl(ml);
+			perfume.setPrecio(precio);
+			perfume.setImagen(imagen);
+			marca.setId(idMarca);
+			perfume.setMarca(marca);
+			
+			
+			
+			Set<ConstraintViolation<Perfume>> violations = validator.validate(perfume);
+			
+			
 			if (violations.isEmpty()) { //no hay errores de validación
 				
-				if (id == 0) {
+				if (idPerfume == 0) {
 					daoPerfume.insert(perfume);
 					message = new Message("success", "El perfume ha sido incorporado con exito al listado");
 				}else {
@@ -116,7 +140,8 @@ public class CrearPerfumeFrontOfficeController extends HttpServlet {
 				}
 				//enviamos los atributos a la vista
 				request.getSession().setAttribute("message", message);
-				response.sendRedirect("inicio");
+				//response.sendRedirect("inicio");
+				view = "inicio";
 				
 			}else { //si hay errores de validacion, me los muestra en el mensaje
 				
@@ -129,26 +154,31 @@ public class CrearPerfumeFrontOfficeController extends HttpServlet {
 				message = new Message("danger", error);
 				
 				//enviamos los atributos a la vista
-				request.setAttribute("perfume", perfume);
-				request.setAttribute("message", message);
+				//request.setAttribute("perfume", perfume);
+				//request.setAttribute("message", message);
 				
 				//ir a la nueva vista
-				request.setAttribute("marcas", daoMarca.getAll() );
-				request.getRequestDispatcher("formulario.jsp").forward(request, response);
+				//request.setAttribute("marcas", daoMarca.getAll() );
+				//request.getRequestDispatcher(view).forward(request, response);
 			}
 			
 			
-		} catch (Exception e) {
+		}catch (SeguridadException e) {
+			LOG.error(" Intentan saltarse la seguridad " + usuario );
+			view = "inicio";
+			
+		}catch (Exception e) {
 			message = new Message("danger", "Lo sentimos, pero ha ocurrido una excepcion, " + e.getMessage());
 			LOG.error(e);;
 			
+		}finally {
 			//enviamos los atributos a la vista
 			request.setAttribute("perfume", perfume);
 			request.setAttribute("message", message);
 			request.setAttribute("marcas", daoMarca.getAll() );
 			
 			//ir a la nueva vista
-			request.getRequestDispatcher("formulario.jsp").forward(request, response);
+			request.getRequestDispatcher(view).forward(request, response);
 		}
 
 		
